006a74e4973e68c3ed5cf873b89bccbb52d4e47c
Alfathurmuhamadzidan2 <129394518+Alfathurmuhamadzidan2@users.noreply.github.com>
2023-04-13 23:43:00 +0700

    Add files via upload

---
+++ movie-database-mainnn/src/pages/Home.js
@@ -0,0 +1,29 @@
+import Navbar from '../components/Navbar/Navbar.js';
+import Hero from '../components/Hero/Hero.js';
+import Movies from '../components/Movies/Movies.js';
+import Footer from '../components/Footer/Footer.js';
+import From from '../components/From/From.js';
+import { useState } from 'react';
+import data from "../components/utils/constants/data.js"
+const Main = () => {
+  // state 
+  const[movies, setMovies] = useState(data)
+  return (
+    <main>
+      <Hero />
+      <Movies movies ={movies} setMovies={setMovies} />
+      <From movies ={movies} setMovies={setMovies}/>
+    </main>
+  );
+};
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
